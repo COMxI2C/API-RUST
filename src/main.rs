b44a5bc71d21821ace8bb5f38faee6a2be9b3d53
lokoi2c <charleesjobs78@gmail.com>
2025-08-26 23:50:19 -0500

    Conection state in starup->main

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,22 @@
 use std::net::TcpListener;
 use api::startup::run;
 use api::configuration::get_configuration;
+use sqlx::PgPool;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     // Panic si falla la lectura de configuración
     let configuration = get_configuration().expect("Failed to read configuration.");
+    //create the instance Pgpool
+    let conection_pool = PgPool::connect(&configuration.database.connection_string())
+    .await
+    .expect("Failed to connect to postgres!!!");
     
     // Ya no usamos el puerto hard-coded 8000
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, conection_pool)?.await?;
+    Ok(())
 }
 //comenario
 
--- src/startup.rs
+++ src/startup.rs
@@ -2,14 +2,20 @@
 
 use actix_web::{web, App, HttpServer};
 use std::net::TcpListener;
-
+use actix_web::web::Data;
 use crate::routes::{health_check, subscriptions};
+//use actix_web::dev::Server;
+//use sqlx::PgConnection;
+use sqlx::PgPool;//PgPll
 
-pub fn run(listener: TcpListener) -> Result<actix_web::dev::Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<actix_web::dev::Server, std::io::Error> {
+    let db_pool = Data::new(db_pool);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscriptions))
+            //register the connection as part of the aplication state
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
